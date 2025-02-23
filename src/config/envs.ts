f5f6f88cf653478d781cf1e83b1a308d0fcd5267
Elton Chavez <elton.mct@gmail.com>
2025-02-23 19:08:55 -0300

    Add NATS configuration and update environment variables

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -4,16 +4,21 @@ import * as joi from 'joi';
 interface EnvVars {
   PORT: number;
   DATABASE_URL: string;
+  NATS_SERVERS: string[];
 }
 
 const envsSchema = joi
   .object({
     PORT: joi.number().required(),
     DATABASE_URL: joi.string().required(),
+    NATS_SERVERS: joi.array().items(joi.string()).required(),
   })
   .unknown(true);
 
-const result = envsSchema.validate(process.env);
+const result = envsSchema.validate({
+  ...process.env,
+  NATS_SERVERS: process.env.NATS_SERVERS?.split(','),
+});
 const error = result.error;
 const value = result.value as EnvVars;
 
@@ -26,4 +31,5 @@ const envVars: EnvVars = value;
 export const envs = {
   port: envVars.PORT,
   databaseUrl: envVars.DATABASE_URL,
+  natsServers: envVars.NATS_SERVERS,
 };
