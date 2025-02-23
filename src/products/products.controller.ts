a6036a160da683ed82957b7019f4a48793b3d122
Elton Chavez <elton.mct@gmail.com>
2025-02-23 00:13:06 -0300

    validate products method added

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -33,4 +33,10 @@ export class ProductsController {
   remove(@Payload('id', ParseIntPipe) id: number) {
     return this.productsService.remove(id);
   }
+
+  @MessagePattern({ cmd: 'validate_products' })
+  validateProducts(@Payload() ids: number[]) {
+    console.log(ids);
+    return this.productsService.validateProducts(ids);
+  }
 }
